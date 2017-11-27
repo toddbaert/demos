fbaf4debe4fae76989a2f97ce343bf365203b2e5
Todd Baert <toddbaert@gmail.com>
2017-11-27 07:06:16 -0500

    adding redistribution-delay to broker config, adding thread count args to failover demo

--- amq/nms/nms-failerover-demo/Program.cs
+++ amq/nms/nms-failerover-demo/Program.cs
@@ -9,49 +9,49 @@ namespace nms_client_demo
     {
         static void Main(string[] args)
         {      
-            Uri connecturi1 = new Uri("tcp://localhost:61616");         
-            Uri connecturi2 = new Uri("tcp://localhost:61617");         
-            Uri failoveruri = new Uri("activemq:failover:tcp://localhost:61616,tcp://localhost:61617");         
 
-            Thread producerThread = new Thread(new ThreadStart(RunProducer));
-            producerThread.Start();
-            Thread consumerThread = new Thread(new ThreadStart(RunConsumer));
-            consumerThread.Start();
+            int numProducers = args[0] != null ? Int32.Parse(args[1]) : 1;
+            int numConsumers = args[1] != null ? Int32.Parse(args[1]) : 1;           
 
-                        Thread consumerThread1 = new Thread(new ThreadStart(RunConsumer));
-            consumerThread1.Start();
-
-                        Thread consumerThread2 = new Thread(new ThreadStart(RunConsumer));
-            consumerThread2.Start();
-
-                        Thread consumerThread3 = new Thread(new ThreadStart(RunConsumer));
-            consumerThread3.Start();
-
-
-            void RunProducer()
+            Uri failoveruri = new Uri("activemq:failover:tcp://localhost:61616,tcp://localhost:61617");     
+            
+            for (int i=0; i<numProducers; i++)
             {
+                var id = i;
+                Thread producerThread = new Thread(() => RunProducer(id));
+                producerThread.Start();
+                Thread.Sleep(1000);
+            }
+
+            for (int i=0; i<numConsumers; i++)
+            {
+                var id = i;
+                Thread consumerThread = new Thread(() => RunConsumer(id));
+                consumerThread.Start();
+                Thread.Sleep(1000);
+            }
+
+            void RunProducer(int id)
+            {
+                Console.WriteLine("Producer " + id + " is connecting...");
                 IConnectionFactory factory = new NMSConnectionFactory(failoveruri);
         
                 using(IConnection connection = factory.CreateConnection())
                 {
-                    connection.ClientId = "sample-producer";            
+                    connection.ClientId = "producer" + id;            
                     using(ISession session = connection.CreateSession())
                     {                
 
-                        // One topic for store 123 on address "orders"
                         ITopic ordersTopic = SessionUtil.GetTopic(session, "topic://orders");        
 
                         using(IMessageProducer olineOrderProducer = session.CreateProducer())
                         {
-                            // Start the connection so that messages will be processed.
                             connection.Start();
 
-                            // Set message reliability
                             olineOrderProducer.DeliveryMode = MsgDeliveryMode.Persistent;
             
                             while (true)
                             {
-                                // Send a message to each store topic
                                 olineOrderProducer.Send(ordersTopic, session.CreateTextMessage("order placed!"));
                                 Thread.Sleep(1000);
                             }
@@ -60,22 +60,21 @@ namespace nms_client_demo
                 }        
             }    
         
-            void RunConsumer()
+            void RunConsumer(int id)
             {
+                Console.WriteLine("Consumer " + id + " is connecting...");
+
                 IConnectionFactory factory = new NMSConnectionFactory(failoveruri);
         
                 using(IConnection connection = factory.CreateConnection())
                 {
-                    connection.ClientId = Guid.NewGuid().ToString();            
+                    connection.ClientId = "consumer" + id;            
                     using(ISession session = connection.CreateSession())
                     {                
-
-                        // One topic for store 123 on address "orders"
                         ITopic ordersTopic = SessionUtil.GetTopic(session, "topic://orders");        
 
                         using(IMessageConsumer someConsumer = session.CreateDurableConsumer(ordersTopic, "orders-consumer", null, false))
                         {
-                            // Start the connection so that messages will be processed.
                             connection.Start();
             
                             while (true)
